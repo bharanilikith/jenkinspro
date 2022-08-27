014aede26e39faaffdc92c2aacfd18a70bff700e
Bharanilikith <Bharanilikith@Bharani>
2022-08-27 17:59:16 +0530

    Hi

---
+++ src/test/java/scripts/Testcase5.java
@@ -0,0 +1,12 @@
+package scripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Testcase5 {
+	@Test
+	public void tc5() {
+		Reporter.log("hello python",true);
+	}
+
+}
